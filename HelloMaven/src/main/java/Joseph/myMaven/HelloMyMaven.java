444ba6ecfb672d6f0dfd047063166de5eeaa72ce
Joseph You <joseph420yu@gmail.com>
2017-08-24 00:14:10 +0800

    My first maven.

---
+++ HelloMaven/src/main/java/Joseph/myMaven/HelloMyMaven.java
@@ -0,0 +1,11 @@
+package Joseph.myMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloMyMaven{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
